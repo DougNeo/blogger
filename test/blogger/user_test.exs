e4214646c3a878bb423c418d44a07c936bd886e5
Douglas Silva <doug.neo@gmail.com>
2022-06-21 02:42:24 -0300

    WIP - testes de user

---
+++ test/blogger/user_test.exs
@@ -0,0 +1,8 @@
+defmodule Blogger.UserTest do
+  use Blogger.ConnCase, async: true
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset" do
+    end
+  end
+end
