7ea3c0aa52f9d31585e97262a84ab5d186e69a51
Douglas Silva <doug.neo@gmail.com>
2022-06-24 01:46:28 -0300

    criado as funções que envia os dados pro front-end da api

--- lib/blogger_web/controller/users_controller.ex
+++ lib/blogger_web/controller/users_controller.ex
@@ -1,19 +1,46 @@
 defmodule BloggerWeb.UsersController do
   use BloggerWeb, :controller
 
-  def create(params) do
-    # ...
+  alias Blogger.User
+  alias Blogger.Users
+
+  def create(conn, params) do
+    with {:ok, %User{} = user} <- Users.Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 
-  def index(params) do
-    # ...
+  # def index(params) do
+  #   with {:ok, %User{} = user} <- Users.Get.by_id(params) do
+  #     conn
+  #     |> put_status(:ok)
+  #     |> render("user.json", user: user)
+  #   end
+  # end
+
+  def show(conn, params) do
+    with {:ok, %User{} = user} <- Users.Get.by_id(params) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
+    end
   end
 
-  def show(params) do
-    # ...
+  def destroy(conn, %{"id" => id}) do
+    with {:ok, %User{} = user} <- Users.Delete.call(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
   end
 
-  def destroy(params) do
-    # ...
+  def update(conn, params) do
+    with {:ok, %User{} = user} <- Users.Update.call(params) do
+      conn
+      |> put_status(:ok)
+      |> render("user.json", user: user)
+    end
   end
 end
