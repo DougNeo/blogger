f9a956028cbb278f6994aad141b1172bd217d890
Douglas Silva <doug.neo@gmail.com>
2022-06-24 01:45:44 -0300

    alterado o changeset para aceitar um ou dois parametros

--- lib/blogger/user.ex
+++ lib/blogger/user.ex
@@ -20,8 +20,8 @@ defmodule Blogger.User do
     timestamps()
   end
 
-  def changeset(params) do
-    %__MODULE__{}
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
     |> cast(params, @fields)
     |> validate_required(@required_params)
     |> validate_length(:display_name, min: 8)
